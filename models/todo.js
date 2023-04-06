df21537e6af278b422659c6013cfd3c143940888
Aryan Soni <aryansoni1593@gmail.com>
2023-04-06 10:46:38 +0530

    first commit

---
+++ models/todo.js
@@ -0,0 +1,30 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Todo extends Model {
+    // eslint-disable-next-line no-unused-vars
+    static associate(models) {
+      // define association here
+    }
+
+    static addTodo({ title, dueDate }) {
+      return this.create({ title: title, dueDate: dueDate, completed: false });
+    }
+
+    markAsCompleted() {
+      return this.update({ completed: true });
+    }
+  }
+  Todo.init(
+    {
+      title: DataTypes.STRING,
+      dueDate: DataTypes.DATEONLY,
+      completed: DataTypes.BOOLEAN,
+    },
+    {
+      sequelize,
+      modelName: "Todo",
+    }
+  );
+  return Todo;
+};
